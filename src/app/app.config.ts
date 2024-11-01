ae60335f06e901a7a94d38f4c84918983e24fa00
Alejandro Sanchez <88404850+alejoRegxi@users.noreply.github.com>
2024-11-01 15:04:37 -0300

    estructura base para el sitio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,7 +25,7 @@ export const appConfig: ApplicationConfig = {
       }),
       withEnabledBlockingInitialNavigation(),
       withViewTransitions(),
-      withHashLocation()
+      // withHashLocation()
     ),
     importProvidersFrom(SidebarModule, DropdownModule),
     IconSetService,
