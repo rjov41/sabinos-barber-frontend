bc2a7494892e1cd863c5644bfa62b53e62254221
Alejandro Sanchez <88404850+alejoRegxi@users.noreply.github.com>
2024-11-05 22:40:40 -0300

    test github pages

---
+++ docs/chunk-GM7BSZJR.js
@@ -0,0 +1,1 @@
+import"./chunk-MKQW6NQN.js";var t=[{path:"",loadComponent:()=>import("./chunk-OXAEAX57.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
---
+++ docs/chunk-SVXFVACB.js
@@ -0,0 +1,1 @@
+import"./chunk-MKQW6NQN.js";var o=[{path:"",loadComponent:()=>import("./chunk-5I6K5HJN.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
